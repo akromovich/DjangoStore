064fb68ff0ea653bf6c778c481064bbced010eff
akromovich <akromovich0928@gmail.com>
2023-07-23 18:33:13 +0500

    first commit

---
+++ products/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def index(request):
+    return render(request, "products/index.html")
+
+
+def product_func(request):
+    return render(request, "products/products.html")
